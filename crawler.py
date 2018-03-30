16988c873ba5b242caed0475164ff1e0b2429cef
Andrew Zhang <alz2@illinois.edu>
2018-03-30 12:01:15 -0500

    added support for discovered links

--- crawler.py
+++ crawler.py
@@ -4,14 +4,33 @@ from bs4 import BeautifulSoup
 class Crawler:
     
     """
+    Constructor 1:
+    Use this if you have a fixed list of links to crawl
         Params:
-            links -- list of links for the crawler to visit
-            soup_analysis_fn -- function which takes in a BeautifulSoup as a parameter.
-                                Once connection is made and html is parsed into a BeautifulSoup by crawler, soup_analysis_fn will be called on the soup
+            links 
+                -- list of links for the crawler to visit
+            soup_analysis_fn 
+                -- function which takes in a BeautifulSoup as a parameter.
+                -- Once connection is made and html is parsed into a BeautifulSoup by crawler
+                   soup_analysis_fn will be called on the soup
     """
     def __init__(self, links, soup_analysis_fn):
         self.links = links
         self.soup_analysis_fn = soup_analysis_fn 
+        self.visited = set()
+
+    """
+    Constructor 1:
+    Use this if you have a dynamic list of links to crawl. Call (your_crawler).push_links_front(your_link)
+    in a function and then set that function as the crawler soup_analysis_fn
+        Params:
+            links 
+                -- initial list of links for the crawler to visit
+        
+    """
+    def __init__(self, links):
+        self.links = links
+        self.soup_analysis_fn = None
         self.visited = set()
 
     def crawl(self, timeout):
@@ -21,16 +40,22 @@ class Crawler:
         while len(self.links) != 0:
             l = self.links.pop()
 
-            if l in set(): # don't repeate links
+            if l in self.visited: # don't repeate links
                 continue
 
             conn = urllib.request.urlopen(l, timeout=timeout)
             if conn is None:
                 print(l, "TIMEOUT")
                 continue
+
             self.visited.add(l);
             soup = BeautifulSoup(conn, "html.parser" )
-            self.soup_analysis_fn(soup)
+            
+            if self.soup_analysis_fn is not None:
+                self.soup_analysis_fn(soup)
 
+    """
+        Pushes link to front of the queue
+    """
     def push_links_front(self, link):
         self.links.append(link)
--- test_crawl.py
+++ test_crawl.py
@@ -2,15 +2,18 @@ from crawler import Crawler
 
 titles = []
 
+# visit these links
+links = ["https://www.google.com", "https://www.yahoo.com"]
+crawler = Crawler(links)
+
 # example analysis function -- for the project we may have to do inverted index calculations here
 def print_soup_title(soup):
     titles.append(soup.title.string)
     print(soup.title.string)
+    crawler.push_links_front("https://docs.python.org/3.1/tutorial/datastructures.html")
+    
+crawler.soup_analysis_fn = print_soup_title;
 
-
-# visit these links
-links = ["https://www.google.com", "https://www.yahoo.com/"]
-crawler = Crawler(links, print_soup_title)
 crawler.crawl(20) # timeout 20 seconds
 
 
