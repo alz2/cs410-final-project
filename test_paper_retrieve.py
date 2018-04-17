ce3a7aaa1e2c30a14b1af8f5390ebd78b2f702d5
Andrew Zhang <alz2@linux-a2.ews.illinois.edu>
2018-04-17 16:57:54 -0500

    crawled prof zhai and another

--- test_paper_retrieve.py
+++ test_paper_retrieve.py
@@ -1,6 +1,6 @@
 from paper_retriever import PaperRetriever
 
-profs = ["Lui Sha", "Margaret Fleck"]
+profs = ["Lui Sha", "Chengxiang Zhai", "Margaret Fleck"]
 retriever = PaperRetriever(profs, "parsed_prof_papers.json", None, num_threads=3) # num_threads=4 causes 503s :(
 results = retriever.retrieve()
 print(results)
