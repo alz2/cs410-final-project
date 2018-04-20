31742141d4d440d474a3d7ddc556ed552df3855d
Andrew Zhang <alz2@illinois.edu>
2018-04-20 12:07:37 -0500

    crawling all profs with scolarly

--- test_paper_retrieve.py
+++ test_paper_retrieve.py
@@ -4,7 +4,13 @@ from time import gmtime, strftime
 history_file = "parsed_prof_papers.json"
 save_as_file = "papers_" + strftime("%Y-%m-%d_%H:%M:%S", gmtime()) + ".json"
 
-profs = ["Lui Sha", "Chengxiang Zhai", "Margaret Fleck"]
+
+profs = []
+profs_file = 'professors.txt'
+with open(profs_file, 'r') as pfile:
+    for p in pfile:
+        profs.append(p)
+
 retriever = PaperRetriever(profs, save_as_file, history_file, num_threads=3) # num_threads=4 causes 503s :(
 results = retriever.retrieve()
 print(results)
