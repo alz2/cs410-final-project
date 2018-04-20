b44ce4aba60917f2e90298e3fa972f6696b9774e
Andrew Zhang <alz2@illinois.edu>
2018-04-20 11:14:26 -0500

    added timestamp to retrieve

--- test_paper_retrieve.py
+++ test_paper_retrieve.py
@@ -1,6 +1,10 @@
 from paper_retriever import PaperRetriever
+from time import gmtime, strftime
+
+history_file = "parsed_prof_papers.json"
+save_as_file = "papers_" + strftime("%Y-%m-%d_%H:%M:%S", gmtime()) + ".json"
 
 profs = ["Lui Sha", "Chengxiang Zhai", "Margaret Fleck"]
-retriever = PaperRetriever(profs, "parsed_prof_papers.json", None, num_threads=3) # num_threads=4 causes 503s :(
+retriever = PaperRetriever(profs, save_as_file, history_file, num_threads=3) # num_threads=4 causes 503s :(
 results = retriever.retrieve()
 print(results)
