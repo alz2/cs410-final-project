32d221a632d632e3638c7af07916ebae729c9ed6
Andrew Zhang <alz2@illinois.edu>
2018-05-06 17:04:26 -0500

    changed to escaped tabs and also to overwrite prevoius files

--- research_papers/convert_json_metapy.py
+++ research_papers/convert_json_metapy.py
@@ -7,8 +7,8 @@ with io.open('../data/papers_2018-05-03_01-58-24.json', 'r', encoding="utf-8") a
     my_dict = json.loads(json_file.read()) #read file into memory.
 
 
-corpus_file = open("prof_data/prof_data.dat", "a", encoding="utf-8") #file to store individual documents. 
-metadata_file = open("prof_data/metadata.dat", "a", encoding="utf-8") #file to store metadata about individual documents.
+corpus_file = open("prof_data/prof_data.dat", "w+", encoding="utf-8") #file to store individual documents. 
+metadata_file = open("prof_data/metadata.dat", "w+", encoding="utf-8") #file to store metadata about individual documents.
 
 
 
@@ -39,7 +39,7 @@ for curPerson in my_dict.keys():
        # doc_content = "Text for doc number {} of Professor {} goes here".format(i, curPerson) #placeholder for actual document content
 
         
-        new_doc_metadata = "{}	{}	{}\n".format(research_info[0], research_info[1], docText[:500])     #important: tab needed to delinate schema of each file for metadata.dat.
+        new_doc_metadata = "{}\t{}\t{}\n".format(research_info[0], research_info[1], docText[:500])     #important: tab needed to delinate schema of each file for metadata.dat.
                                                                                             #only store the first 1000 characters of data in the metadata file. 
         corpus_file.write(new_doc) #write as a document. one doc per line
         metadata_file.write(new_doc_metadata)
