d25a5bb53f3375ba4f3ce7d0e36b4ad2063669d5
Andrew Zhang <alz2@illinois.edu>
2018-05-10 20:36:12 -0500

    fixed description error

--- website/app.py
+++ website/app.py
@@ -52,7 +52,7 @@ def get_matching_docs(txtToFind, page_no, numberOfResults = RESULTS_PER_PAGE):
 """
 def get_peripheral(corpus_words, target_words, n):
     corpus_words_lower = [t.lower() for t in corpus_words]
-    attempt = 0
+    attempt = 0 # index to begin search
     found = False
     target_occur = -1
     while not found:
@@ -63,7 +63,7 @@ def get_peripheral(corpus_words, target_words, n):
         # occurance of first string in target
         toff = 1
         found_seq = True
-        for ti in range(1, len(target_words)):
+        for ti in range(1, len(target_words)): # search next words to see if they match query
             if target_occur + ti >= len(corpus_words_lower): # no more words in corpus
                 return (-1 , -1)
 
@@ -82,10 +82,10 @@ def get_peripheral(corpus_words, target_words, n):
 
 
 def add_emphasis(corpus, target):
-    lower_target = target.lower()
+    lower_target = stem_string(target.lower())
     corpus_words = corpus.split(" ")
     for cwi in range(len(corpus_words)):
-        if corpus_words[cwi].lower() == lower_target:
+        if stem_string(corpus_words[cwi]).lower() == lower_target:
             corpus_words[cwi] = '<strong>' + corpus_words[cwi] + '</strong>'
     return ' '.join(corpus_words)
 
@@ -103,48 +103,62 @@ def get_first_sentence(text):
     Formats results returned by metapy in a format expected by the template results.html
 """
 def format_results(best_docs, query): 
-    # stem query 
-    stemmed_query = stem_string(query).split(" ")
-    if stemmed_query[len(stemmed_query) - 1] == "": # weird split case
-        del stemmed_query[len(stemmed_query) - 1]
+    # stem the queryquery 
+    stemmed_query_wds = stem_string(query).split(" ")
+    if stemmed_query_wds[-1] == "": # weird split case
+        del stemmed_query_wds[-1]
 
     # result (link, title, description)
     formatted = []
     for num, (d_id, _) in enumerate(best_docs):
-        search_result = []
-        #get url, title, and description
-        path = idx.metadata(d_id).get('path')
+        search_result = [] 
+        path = idx.metadata(d_id).get('path') # get the link
         search_result.append(path)
 
-        title = idx.metadata(d_id).get('title')
+        title = idx.metadata(d_id).get('title') # get the title
         search_result.append(title)
         
-        content = idx.metadata(d_id).get('content')
-        stemmed_content = idx.metadata(d_id).get('stemmedcontent').split(" ")
-        si = get_peripheral(stemmed_content, stemmed_query, 15)
+        # figure out the description
+        content_corp = idx.metadata(d_id).get('content') # get the raw content
+        c_doc = metapy.index.Document()
+        c_doc.content(content_corp)
+        c_tok = metapy.analyzers.ICUTokenizer(suppress_tags=True)
+        c_tok.set_content(c_doc.content())
+        content_wds = [t for t in c_tok]
+        
+        # stemmed content
+        stemmed_corp = idx.metadata(d_id).get('stemmedcontent')
+        sc_doc = metapy.index.Document()
+        sc_doc.content(stemmed_corp)
+        sc_tok = metapy.analyzers.ICUTokenizer(suppress_tags=True)
+        sc_tok.set_content(sc_doc.content())
+        stemmed_content_wds = [t for t in sc_tok]
+        si = get_peripheral(stemmed_content_wds, stemmed_query_wds, 15)
+
         if si[0] == -1: # none found for all
-            for w in stemmed_query: # try for each word
-                si = get_peripheral(stemmed_content, [w], 15) # expects a list
-                if si[0] != -1:
+            for sq in stemmed_query_wds: # try for each word
+                si = get_peripheral(stemmed_content_wds, [sq], 15)
+                if si[0] != -1: # found valid peripheral
+                    print('found', sq, 'at', si)
                     break
-            if si[0]  == -1:
+
+            if si[0] == -1:
                 summary = 'Sorry, No Description Available.'
             else:
-                content_words = content.split(" ")
                 query_words = query.split(" ")
-                summary = ' '.join(content_words[si[0]: si[1]])
+                summary = ' '.join(content_wds[si[0]: si[1]])
                 summary += '...'
+                # add emphasis to the query words
                 for q in query_words:
                     summary = add_emphasis(summary, q)
-                summary = Markup(summary)
-        else:
-            content_words = content.split(" ")
+                summary = Markup(summary) # preserve markup tags
+        else: # found both terms next to each other
             query_words = query.split(" ")
-            summary = ' '.join(content_words[si[0]: si[1]])
+            summary = ' '.join(content_wds[si[0]: si[1]])
             summary += '...'
             for q in query_words:
                 summary = add_emphasis(summary, q)
-            summary = Markup(summary)
+            summary = Markup(summary) # preserve markup tags
 
         search_result.append(summary)
         formatted.append(search_result)
